8c2b9ab3efc90f832a4c71ea9958483e55afef8d
𝑰𝒏̃𝒂𝒌𝒊 𝒁𝒂́𝒓𝒂𝒕𝒆 愛 <zaarate7ignacio@gmail.com>
2025-07-11 15:24:43 -0400

    Agrupación de PostIt.

--- src/components/PostItList.jsx
+++ src/components/PostItList.jsx
@@ -1,22 +1,19 @@
 import React from "react";
 import PostIt from "./PostIt";
+import '../styles/App.css';
 
 function PostItList({ notes, onDelete }) {
     return (
-        <div className="container">
-            <div className="row justify-content-start">
-                {notes.map((note, index) => (
-                    <div 
-                        key={index} 
-                        className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4 d-flex justify-content-center"
-                        sstyle={{ display: 'flex', justifyContent: 'center' }}>
-
-                        <PostIt 
-                            title={note.title} 
-                            description={note.description} important={note.important} onDelete={() => onDelete(index)}/>
-                    </div>
-                ))}
-            </div>
+        <div className="postit-grid">
+            {notes.map((note, index) => (
+                <PostIt
+                    key={index}
+                    title={note.title}
+                    description={note.description}
+                    important={note.important}
+                    onDelete={() => onDelete(index)}
+                />
+            ))}
         </div>
     );
 }
