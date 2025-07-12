83c78f115361a13493cb91a452800b79469855c6
𝑰𝒏̃𝒂𝒌𝒊 𝒁𝒂́𝒓𝒂𝒕𝒆 愛 <zaarate7ignacio@gmail.com>
2025-07-11 20:55:54 -0400

    Reintegración de código sin CSS.

--- src/components/postit.jsx
+++ src/components/postit.jsx
@@ -1,38 +1,12 @@
 import React from "react";
-import '../styles/App.css';
 
-function PostIt({ title, description, important, onDelete }) {
-    const backgroundColor = important ? '#EC7063' : '#FFFFCC';
-
+function Postit({id, texto, eliminarPostit}) {
     return (
-        <div className="postit"
-            style={{
-                backgroundColor,
-                aspectRatio: '1 / 1', // Mantiene el cuadrado
-                width: '100%',        // Se adapta al espacio
-                maxWidth: '250px',    // Limita el tamaño en pantalla
-                boxShadow: '2px 2px 8px rgba(0,0,0,0.3)',
-                borderRadius: '10px',
-                wordBreak: 'break-word',
-                position: 'relative',
-                padding: '1rem'
-            }}
-        >
-            {/* Botón para eliminar el post-it */}
-            <button 
-                onClick={ onDelete } 
-                className="btn-close position-absolute" 
-                style={{top: '10px', right: '10px'}} 
-                aria-label="Eliminar"
-            />
-
-            {/* Título */}
-            {title && <h4 className="mb-2" style={{fontSize: '2rem'}}>{title}</h4>}
-
-            {/* Descripción */}
-            <p style={{ fontSize: '1.2rem', marginBottom: 0}}>{description}</p>
+        <div className="postit">
+            <p>{texto}</p>
+            <button className="btn-eliminar" onClick={() => eliminarPostit(id)}>X</button>
         </div>
-    )
+    );
 }
 
-export default PostIt;
+export default Postit;
