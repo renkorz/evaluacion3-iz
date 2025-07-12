936104443e1a7bc00ffc99d0f04beaea07b4a8f2
𝑰𝒏̃𝒂𝒌𝒊 𝒁𝒂́𝒓𝒂𝒕𝒆 愛 <zaarate7ignacio@gmail.com>
2025-07-11 20:47:32 -0400

    Actualización de archivo completo.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import React, { useState } from "react";
-import PostItForm from './components/PostItForm';
-import PostItList from './components/PostItList';
-import './styles/App.css';
+import React, {use, useState} from "react";
+import Formulariopostit from './components/formulariopostit';
+import Postit from "./components/PostIt";
+import './App.css';
 
 function App() {
-  const [notes, setNotes] = useState([]);
+  const [postits, setPostits] = useState([]);
+  const [contadorId, setContadorId] = useState([1]);
 
-  const addNote = (note) => {
-    setNotes([note, ...notes]);
+  const agregarPostit = (texto) => {
+    const nuevoPostit ={
+      id: contadorId,
+      texto: texto
+    };
+    setPostits([nuevoPostit, ...postits]);
+    setContadorId(contadorId + 1);
   };
 
-  const deleteNote = (index) => {
-    const updateNotes = [...notes];
-    updateNotes.splice(index, 1);
-    setNotes(updateNotes);
+  const eliminarPostit = (id) => {
+    setPostits(postits.filter(postit => postit.id !== id));
   };
 
   return (
-    <div className="App container-fluid py-4">
-      <h1 className='text-center mb-4 title'>Mis Notas</h1>
-      <PostItForm onAdd={addNote} />
-      <PostItList notes={notes} onDelete={deleteNote}/>
+    <div className="app-container">
+      <h1>Mis Notas</h1>
+      <Formulariopostit agregarPostit={agregarPostit}/>
+      <div className="postits-grid">
+        {postits.map(postit => (
+          <Postit
+          key={postit.id}
+          id={postit.id}
+          texto={postit.texto}
+          eliminarPostit={eliminarPostit}
+          />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
